fc820631b653441246bc24e6a59301532c34ba07
Parvin <pervin.guseynov@gmail.com>
2020-07-22 17:31:07 +0400

    Add models and migrate, etc

--- news/models.py
+++ news/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+# id - INT
+# title - Varchar
+# content - Text
+# created_at - Datetime
+# updated_at - Datetime
+# photo - Image
+# is_published - Boolean
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    is_published = models.BooleanField(default=True) # Черновик. Чекбокс отмечен, т.е. по умолчанию новость публикуется
+
+
+
